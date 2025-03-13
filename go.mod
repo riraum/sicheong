fba272dbd39332f890ba0920af5a46106e8fd417
M <114847810+riraum@users.noreply.github.com>
2025-03-13 19:34:58 +0900

    Add webserver
    Fix #3

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/riraum/si-cheong/v2
+
+go 1.24.1
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Server struct {
+}
+
+// type ServeMux struct {
+// 	GET "/"
+// }
+
+func hello(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprint(w, http.StatusOK)
+}
 
 func main() {
 	fmt.Println("Hello si-cheong user")
+
+	mux := http.NewServeMux()
+
+	// rh := http.RedirectHandler("https://brave.com", http.StatusTemporaryRedirect)
+	// http.HandleFunc("/", GETHandler)
+
+	// httpStatus := http.StatusAccepted
+
+	mux.HandleFunc("/", hello)
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
