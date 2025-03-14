e5748c3bbc3bd61435f65d9e89cc6e7b3b698a22
M <114847810+riraum@users.noreply.github.com>
2025-03-14 19:49:43 +0900

    WIP 404 logic

--- http/http.go
+++ http/http.go
@@ -9,12 +9,7 @@ import (
 type Server struct {
 }
 
-func getIndex(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
+func getIndex(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, http.StatusOK)
 }
@@ -29,12 +24,18 @@ func postAPIPosts(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, http.StatusCreated)
 }
 
+func notFound(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, http.StatusNotFound)
+}
+
 func ServeDirs() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", getIndex)
 	mux.HandleFunc("GET /api/v0/posts", getAPIPosts)
 	mux.HandleFunc("POST /api/v0/posts", postAPIPosts)
+	mux.HandleFunc("GET /{$}", notFound)
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
