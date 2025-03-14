0c8cdf8b5230d81e80e655d1002418633958099a
M <114847810+riraum@users.noreply.github.com>
2025-03-14 20:09:39 +0900

    Remove custom 404 handler

--- http/http.go
+++ http/http.go
@@ -24,18 +24,12 @@ func postAPIPosts(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, http.StatusCreated)
 }
 
-func notFound(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, http.StatusNotFound)
-}
-
 func ServeDirs() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", getIndex)
 	mux.HandleFunc("GET /api/v0/posts", getAPIPosts)
 	mux.HandleFunc("POST /api/v0/posts", postAPIPosts)
-	mux.HandleFunc("/", notFound)
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
