2dec36306a1c30a2479bf9981a0509bdd6cb3609
M <114847810+riraum@users.noreply.github.com>
2025-03-14 17:17:02 +0900

    Router for paths and methods
    Fix #7, Fix #8

--- http/http.go
+++ http/http.go
@@ -9,19 +9,32 @@ import (
 type Server struct {
 }
 
-func getRoot(w http.ResponseWriter, _ *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	fmt.Fprint(w, http.StatusOK)
 }
 
-func getAPIPosts(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, http.StatusOK, "[]")
+func apiPosts(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/api/v0/posts" {
+		http.NotFound(w, r)
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprint(w, http.StatusOK, "[]")
+	case http.MethodPost:
+		fmt.Fprint(w, http.StatusCreated)
+	}
 }
 
 func ServeDirs() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", getRoot)
-	mux.HandleFunc("/api/v0/posts", getAPIPosts)
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/api/v0/posts", apiPosts)
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
