43fe98375c16decc3cbbef5ddc8301350b240ebb
M <114847810+riraum@users.noreply.github.com>
2025-03-17 16:56:04 +0900

    Remove comments

--- http/http_test.go
+++ http/http_test.go
@@ -8,16 +8,6 @@ import (
 
 func TestServeMux(t *testing.T) {
 	mux := SetupMux()
-	// mux.HandleFunc("GET /{$}", func(w http.ResponseWriter, _ *http.Request) {
-	// 	w.Write([]byte("GET /"))
-	// })
-	// mux.HandleFunc("GET /api/v0/posts", func(w http.ResponseWriter, _ *http.Request) {
-	// 	w.Write([]byte("GET /api/v0/posts"))
-	// })
-	// mux.HandleFunc("POST /api/v0/posts", func(w http.ResponseWriter, _ *http.Request) {
-	// 	w.Write([]byte("POST /api/v0/posts"))
-	// })
-
 	// Create a testing server with the ServeMux
 	server := httptest.NewServer(mux)
 	defer server.Close()
