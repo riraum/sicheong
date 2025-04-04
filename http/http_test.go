720f1d2a1effede4a642da64e4c07a00fb97d91a
M <114847810+riraum@users.noreply.github.com>
2025-04-04 15:40:15 +0900

    WIP

--- http/http_test.go
+++ http/http_test.go
@@ -3,17 +3,26 @@ package http
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
 func TestServeMux(t *testing.T) {
-	mux := SetupMux()
+	var s Server
+	// s.RootDir = t.TempDir()
+
+	f, err := os.CreateTemp("", "index.html")
+
+	// fname := filepath.Join(s.RootDir, "index.html")
+	// fmt.Printf("fname: %v", fname)
+
+	mux := s.SetupMux()
 	// Create a testing server with the ServeMux
 	ts := httptest.NewServer(mux)
 	defer ts.Close()
 
 	// Test GET request
-	resp, err := http.Get(ts.URL + "/")
+	resp, err := http.Get(ts.URL + f.Name())
 	if err != nil {
 		t.Errorf("Error making GET request: %v", err)
 	}
