062ba97d64c77dff8f3ba881bfed8d2a0a7e0473
M <114847810+riraum@users.noreply.github.com>
2025-06-21 13:39:03 +0900

    simplify initial server struct

--- main.go
+++ main.go
@@ -11,9 +11,10 @@ import (
 )
 
 //go:embed static/*
-var static embed.FS
-
-var t = template.Must(template.ParseFS(static, "static/*"))
+var (
+	static embed.FS
+	t      = template.Must(template.ParseFS(static, "static/*"))
+)
 
 func main() {
 	log.Print("Hello si-cheong user")
@@ -28,15 +29,16 @@ func main() {
 		log.Fatalf("Failed to create new db %v", err)
 	}
 
+	s := http.Server{
+		EmbedRootDir: static,
+		DB:           d,
+		Template:     t,
+		Key:          key,
+	}
+
 	if err = d.Fill(); err != nil {
 		log.Fatalf("error filling posts into db: %v", err)
 	}
-
-	var s http.Server
-	s.EmbedRootDir = static
-	s.DB = d
-	s.Template = t
-	s.Key = key
 
 	mux := s.SetupMux()
 	http.Run(mux)
