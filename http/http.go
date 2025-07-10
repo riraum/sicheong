bff38bcf8715e68fea8ca2306ccbea035a206930
M <114847810+riraum@users.noreply.github.com>
2025-07-10 15:10:06 +0900

    add port to server struct

--- http/http.go
+++ http/http.go
@@ -53,11 +53,9 @@ func Run(mux *http.ServeMux) {
 		ReadTimeout:  5 * time.Second,  //nolint:all
 		WriteTimeout: 10 * time.Second, //nolint:all
 		Handler:      mux,
+		Addr:         ":8081",
 	}
 	log.Println(srv.ListenAndServe())
-	// if err := (http.ListenAndServe(":8081", mux)); err != nil {
-	// 	log.Fatalf("failed to http serve: %s", err)
-	// }
 }
 
 func (s Server) authenticated(r *http.Request) (db.Author, bool, error) {
