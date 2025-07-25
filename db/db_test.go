eedac5969b184c8002bee331daf4585db4cfd5fe
M <114847810+riraum@users.noreply.github.com>
2025-07-25 16:41:26 +0900

    adjust test

--- db/db_test.go
+++ db/db_test.go
@@ -1,8 +1,8 @@
 package db
 
 import (
-	"log"
 	"os"
+	"path/filepath"
 	"testing"
 
 	_ "github.com/mattn/go-sqlite3" //revive be gone
@@ -14,82 +14,85 @@ import (
 // 	}
 // }
 
-func TestAll(t *testing.T) {
-	testFile, err := os.CreateTemp("", "testFile")
-	if err != nil {
-		log.Fatal(err)
+func TestTest(t *testing.T) {
+	testPath := filepath.Join(t.TempDir(), "file")
+	if err := os.WriteFile(testPath, []byte("test"), 0644); err != nil {
+		t.Fatalf("err writing: %v", err)
 	}
 
-	defer os.Remove(testFile.Name())
-
-	_, err = testFile.Write([]byte{1, 2, 3})
+	byte, err := os.ReadFile(testPath)
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("err reading: %v", err)
 	}
 
-	log.Print(testFile)
-
-	// testDB := filepath.Join(t.TempDir(), "test.db")
-
-	// d, err := New(testDB)
-	// log.Print(testDB)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// // _, err = d.client.Query("select ID, Date, Title, Link, Content, Author from posts")
-	// // if err != nil {
-	// // 	t.Errorf("error selecting rows %v", err)
-	// // }
-
-	// if err = d.Fill(); err != nil {
-	// 	t.Errorf("error filling db: %v", err)
-	// }
-
-	// par := Params{
-	// 	// Sort:      "date",
-	// 	// Direction: "asc",
-	// 	// Author:      "",
-	// }
-
-	// got, err := d.ReadPosts(par)
-	// if err != nil {
-	// 	t.Errorf("error getting rows: %v", err)
-	// }
-
-	// noTimeStamps(got)
-
-	// want := Posts{
-	// 	[]Post{
-	// 		{
-	// 			ID:       2,
-	// 			Date:     1684997010,
-	// 			Title:    "Status 100",
-	// 			Link:     "https://http.cat/status/100",
-	// 			Content:  "Good HTTP status 100 explainer",
-	// 			AuthorID: 2,
-	// 		},
-	// 		{
-	// 			ID:       3,
-	// 			Date:     1727780130,
-	// 			Title:    "Status 301",
-	// 			Link:     "https://http.cat/status/301",
-	// 			Content:  "Good HTTP status 301 explainer",
-	// 			AuthorID: 3,
-	// 		},
-	// 		{
-	// 			ID:       1,
-	// 			Date:     1748000743,
-	// 			Title:    "Status 200",
-	// 			Link:     "https://http.cat/status/200",
-	// 			Content:  "Good HTTP status 200 explainer",
-	// 			AuthorID: 1,
-	// 		},
-	// 	},
-	// }
-
-	// if !reflect.DeepEqual(want, got) {
-	// 	t.Fatalf("expected %v: got: %v", want, got)
-	// }
+	if string(byte) != "test" {
+		t.Fatalf("missmatch data test: %v", err)
+	}
 }
+
+// func TestAll(t *testing.T) {
+
+// testDB := filepath.Join(t.TempDir(), "test.db")
+
+// d, err := New(testDB)
+// log.Print(testDB)
+
+// if err != nil {
+// 	log.Fatal(err)
+// }
+
+// // _, err = d.client.Query("select ID, Date, Title, Link, Content, Author from posts")
+// // if err != nil {
+// // 	t.Errorf("error selecting rows %v", err)
+// // }
+
+// if err = d.Fill(); err != nil {
+// 	t.Errorf("error filling db: %v", err)
+// }
+
+// par := Params{
+// 	// Sort:      "date",
+// 	// Direction: "asc",
+// 	// Author:      "",
+// }
+
+// got, err := d.ReadPosts(par)
+// if err != nil {
+// 	t.Errorf("error getting rows: %v", err)
+// }
+
+// noTimeStamps(got)
+
+// want := Posts{
+// 	[]Post{
+// 		{
+// 			ID:       2,
+// 			Date:     1684997010,
+// 			Title:    "Status 100",
+// 			Link:     "https://http.cat/status/100",
+// 			Content:  "Good HTTP status 100 explainer",
+// 			AuthorID: 2,
+// 		},
+// 		{
+// 			ID:       3,
+// 			Date:     1727780130,
+// 			Title:    "Status 301",
+// 			Link:     "https://http.cat/status/301",
+// 			Content:  "Good HTTP status 301 explainer",
+// 			AuthorID: 3,
+// 		},
+// 		{
+// 			ID:       1,
+// 			Date:     1748000743,
+// 			Title:    "Status 200",
+// 			Link:     "https://http.cat/status/200",
+// 			Content:  "Good HTTP status 200 explainer",
+// 			AuthorID: 1,
+// 		},
+// 	},
+// }
+
+// if !reflect.DeepEqual(want, got) {
+// 	t.Fatalf("expected %v: got: %v", want, got)
+// }
+// }
